e7d9a4fecd322aec07f306b187a1142fa1dbea45
omelenetskiy <AOmele01@kenvue.com>
2024-12-26 15:26:00 +0300

    fix: offscreen

--- offscreen.js
+++ offscreen.js
@@ -6,11 +6,11 @@ chrome.runtime.onMessage.addListener((message) => {
     } else if (message.action === 'pause') {
         audio.pause();
     } else if (message.action === 'setTrack') {
-        audio.src = `music/${message.track}.mp3`; // Убедитесь, что имена файлов совпадают с опциями в HTML
+        audio.src = `music/${message.track}.mp3`;
         audio.load();
         audio.play();
     } else if (message.action === 'syncTrack') {
-        audio.src = `music/${message.track}.mp3`; // Убедитесь, что имена файлов совпадают с опциями в HTML
+        audio.src = `music/${message.track}.mp3`;
         audio.load();
     }
 });
